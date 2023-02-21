b4e7e93e7413989ca8c070297d2f157a8f99b567
Filip <65063262+Dsa-jpg@users.noreply.github.com>
2023-02-21 19:22:58 +0000

    Pending changes exported from your codespace

---
+++ Graf.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+# Načtení dat z CSV souboru
+data = pd.read_csv('word_counts.csv')
+
+# Vytvoření sloupcového grafu s mezerou mezi sloupci
+plt.bar(data['word'], data['count'], width=0.6)
+
+# Nastavení popisků os
+plt.xlabel('Slova')
+plt.ylabel('Počet')
+
+# Zvýraznění každého 5. popisku osy X pro lepší čitelnost
+plt.gca().xaxis.set_major_locator(ticker.MultipleLocator(2))
+
+# Otočení popisků os pro lepší čitelnost
+plt.xticks(rotation=90)
+
+# Zobrazení a uložení grafu
+plt.tight_layout()
+plt.savefig('word_counts_graph.png')
+plt.show()
